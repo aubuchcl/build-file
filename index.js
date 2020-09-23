db0dc4dc350893220528742e5f8387e38c1220b5
Christopher Aubuchon <caubuchon@petrichor.io>
2020-09-22 18:36:43 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require("http");
+// var environmentVars = JSON.stringify(process.env, null, 4);
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("hello world");
+  console.log("Got a connection");
+});
+server.listen(80, "::0");
+// server.listen(80, "0.0.0.0");
+console.log("Server running on localhost at port 80");
+console.log("this server is available via ipv4 and ipv6");
+console.log("made a change");
