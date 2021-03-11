31f08af2865c46033590c3702487d99700a435bd
Chris Aubuchon <aubuchcl@gmail.com>
2021-03-10 16:04:47 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ server.listen(80, "::0");
 console.log("Server running on localhost at port 80");
 console.log("this server is available via ipv4 and ipv6");
 console.log("made a change");
+
+function handleExit(signal) {
+  console.log(`Received ${signal}. Close my server properly.`)
+  server.close(function () {
+    process.exit(0);
+  });
+}
+process.on('SIGINT', handleExit);
+process.on('SIGQUIT', handleExit);
+process.on('SIGTERM', handleExit);
